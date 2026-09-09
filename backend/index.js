1da8d73dea25d66ed551c2df5c0695025ea2b6b8
SANJEEV KUMAR <kumarsanjeev01254@gmail.com>
2026-09-09 22:38:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://jobportal-2-sly8.onrender.com",
+  origin: "http://localhost:5173",
   credentials: true,
 }
 

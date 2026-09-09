397cd5809f98c63faff47d3cdcf8c0a79ce1ffb7
SANJEEV KUMAR <kumarsanjeev01254@gmail.com>
2026-09-09 22:50:10 +0530

    Update data.js

--- frontend/src/utilis/data.js
+++ frontend/src/utilis/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com";
-export const JOB_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com";
-export const APPLICANT_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com";
-export const COMPANY_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com";
+export const USER_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com/api/users";
+export const JOB_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com/api/job";
+export const APPLICANT_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com/api/applicant";
+export const COMPANY_API_ENDPOINT = "https://jobportal-2-sly8.onrender.com/api/company";

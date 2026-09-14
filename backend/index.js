9da3f92425acf175a5aa5a361736e0a0de634fd5
SANJEEV KUMAR <kumarsanjeev01254@gmail.com>
2026-09-14 21:51:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://jobportal-1-5h6e.onrender.com",
   "https://jobportal-2-bguc.onrender.com",
+  "https://jobportal-3-bguc.onrender.com",
 ];
 
 app.use(

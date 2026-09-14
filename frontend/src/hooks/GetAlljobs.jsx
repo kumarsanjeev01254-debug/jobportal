8df5182905aa76201fa3aa51bd5180eee42060c3
SANJEEV KUMAR <kumarsanjeev01254@gmail.com>
2026-09-14 21:42:57 +0530

    Update GetAlljobs.jsx

--- frontend/src/hooks/GetAlljobs.jsx
+++ frontend/src/hooks/GetAlljobs.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 
-import { setAllJobs } from "@/redux/jobslice";
+import { setAllJobs } from "@/redux/Jobslice";
 import { JOB_API_ENDPOINT } from "@/utilis/data";
 
 const useGetAllJobs = (enabled = true) => {
